2b3ae8e464bc759a5e445757b46259e7e82a740a
phish3y <logsdonevan1@gmail.com>
2020-12-06 21:07:34 -0800

    continue

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,77 @@
+use std::env;
 use std::fs::File;
-use std::io::{self, BufRead};
-use::std::path::Path;
+use std::io::{self, BufRead, Read, Write};
+use std::path::Path;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-fn main() {
-    if let Ok(lines) = read_lines("/home/evan/dev/perpendicular-rust/resources/test.csv") {
-        for line in lines {
-            if let Ok(ip) = line {
-                println!("{}", ip);
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args[1].eq_ignore_ascii_case("leader") {
+        println!("I am the leader");
+        if let Ok(lines) = read_lines("/home/evan/dev/perpendicular-rust/resources/test.csv") {
+            let lines_per_node = 5;
+            let mut vec_lines = vec![];
+            for line in lines {
+                if let Ok(ip) = line {
+                    vec_lines.push(ip);
+                }
             }
+
+            match TcpStream::connect("54.187.129.214:4999") {
+                Err(e) => println!("Error: {}", e),
+                Ok(mut stream) => {
+                    println!("Connected!");
+                    for line in vec_lines {
+                        stream.write(line.as_bytes()).unwrap();
+                    }
+                }
+            }
+        } else {
+            println!("Failed to find file!");
         }
     } else {
-        println!("Failed to find file!");
+        println!("I am a worker");
+        let listener = TcpListener::bind("0.0.0.0:4999")?;
+
+        for streams in listener.incoming() {
+            thread::spawn(move || {
+                match streams {
+                    Err(e) => println!("Error: {}", e),
+                    Ok(stream) => handle_stream(stream).unwrap()
+                }
+            });
+        }
     }
+
+    Ok(())
+}
+
+fn handle_stream(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    println!("Connection from: {}", stream.peer_addr()?);
+
+    let mut buffer = [0; 8192];
+    //loop {
+        let nbytes = stream.read(&mut buffer)?;
+        if nbytes == 0 {
+            return Ok(());
+        }
+
+        for i in 0..nbytes {
+            match std::str::from_utf8(&buffer) {
+                Err(e) => println!("Error: {}", e),
+                Ok(result) => {
+                    print!("{}", result);
+                }
+            }
+        }
+        println!("");
+
+        stream.flush()?;
+    //}
+
+    Ok(())
 }
 
 
